80356b7ff4f1d9c57b317f6985715e46e3a52ef7
HeaoYe <1697427528@qq.com>
2026-09-03 22:21:48 +0800

    第20讲：BVH构建优化（详见第10讲commit）

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,83 @@
-use cpu_path_tracing::{camera, geometry, integrator, light_sampler, material, scene};
+use cpu_path_tracing::{camera, geometry, integrator, light_sampler, material, scene, util};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let film = camera::Film::new(192 * 10, 108 * 10);
-    let camera_pos = glam::vec3(0.0, 37.0, -61.0);
-    let mut camera = camera::Camera::new(film, camera_pos, glam::vec3(0.0, 8.0, 0.0), 16.0);
+    let mut camera = camera::Camera::new(
+        film,
+        glam::vec3(0.0, 1.25, -6.0),
+        glam::vec3(0.0, 1.95, 0.0),
+        45.0,
+    );
 
     let mut builder = scene::SceneBuilder::default();
 
-    let triangles = [
-        geometry::Triangle::from_points(
-            glam::vec3(-17.0, 0.0, -1.5),
-            glam::vec3(-17.0, 0.0, 1.5),
-            glam::vec3(17.0, 0.0, 1.5),
+    let model = geometry::Model::load("models/buddha.obj")?;
+    builder.add_shape(
+        &model,
+        material::Material::specular(util::Rgb::new(241, 191, 79)),
+        scene::InstanceTransform {
+            translation: glam::vec3(-3.0, 1.75, 0.0),
+            scale: glam::vec3(4.0, 4.0, 4.0),
+            ..Default::default()
+        },
+    );
+    builder.add_shape(
+        &model,
+        material::Material::conductor_with_alpha([1.2, 1.2, 5.3], [3.4, 3.4, 2.1], 0.8, 0.2),
+        scene::InstanceTransform {
+            translation: glam::vec3(-1.0, 1.75, 0.0),
+            scale: glam::vec3(4.0, 4.0, 4.0),
+            ..Default::default()
+        },
+    );
+    builder.add_shape(
+        &model,
+        material::Material::dielectric_with_alpha(
+            1.4,
+            glam::Vec3::ONE,
+            util::Rgb::new(180, 180, 154),
+            0.1,
+            0.3,
         ),
-        geometry::Triangle::from_points(
-            glam::vec3(-17.0, 0.0, -1.5),
-            glam::vec3(17.0, 0.0, 1.5),
-            glam::vec3(17.0, 0.0, -1.5),
-        ),
-    ];
-    let light_sphere_1 = geometry::Sphere {
-        center: glam::vec3(-15.0 + 00.0 / 3.0, 12.0, 8.0),
-        radius: 2.0,
-    };
-    let light_sphere_2 = geometry::Sphere {
-        center: glam::vec3(-15.0 + 30.0 / 3.0, 12.0, 8.0),
+        scene::InstanceTransform {
+            translation: glam::vec3(1.0, 1.75, 0.0),
+            scale: glam::vec3(4.0, 4.0, 4.0),
+            ..Default::default()
+        },
+    );
+    builder.add_shape(
+        &model,
+        material::Material::diffuse(util::Rgb::new(241, 191, 79)),
+        scene::InstanceTransform {
+            translation: glam::vec3(3.0, 1.75, 0.0),
+            scale: glam::vec3(4.0, 4.0, 4.0),
+            ..Default::default()
+        },
+    );
+
+    let sphere = geometry::Sphere {
+        center: glam::Vec3::ZERO,
         radius: 1.0,
     };
-    let light_sphere_3 = geometry::Sphere {
-        center: glam::vec3(-15.0 + 60.0 / 3.0, 12.0, 8.0),
-        radius: 0.5,
-    };
-    let light_sphere_4 = geometry::Sphere {
-        center: glam::vec3(-15.0 + 90.0 / 3.0, 12.0, 8.0),
-        radius: 0.1,
-    };
-    builder.add_area_light(
-        &light_sphere_1,
-        material::Material::default(),
-        scene::InstanceTransform::default(),
-        glam::Vec3::splat(1.0),
-        false,
+    builder.add_shape(
+        &sphere,
+        material::Material::specular(glam::Vec3::ONE),
+        scene::InstanceTransform {
+            translation: glam::vec3(0.0, 3.75, 3.0),
+            ..Default::default()
+        },
     );
-    builder.add_area_light(
-        &light_sphere_2,
-        material::Material::default(),
-        scene::InstanceTransform::default(),
-        glam::Vec3::splat(4.0),
-        false,
-    );
-    builder.add_area_light(
-        &light_sphere_3,
-        material::Material::default(),
-        scene::InstanceTransform::default(),
-        glam::Vec3::splat(16.0),
-        false,
-    );
-    builder.add_area_light(
-        &light_sphere_4,
-        material::Material::default(),
-        scene::InstanceTransform::default(),
-        glam::Vec3::splat(400.0),
-        false,
-    );
-    let light_pos_center = glam::vec3(0.0, 12.0, 8.0);
-    let alphas = [0.4, 0.25, 0.16, 0.04];
-    for (i, &alpha) in alphas.iter().enumerate() {
-        let theta = (i as f32 * 15.0).to_radians();
-        let center = glam::vec3(0.0, 17.0 * (1.0 - theta.cos()), 17.0 * theta.sin());
-        let normal = ((light_pos_center - center).normalize() + (camera_pos - center).normalize())
-            .normalize();
-        let rotation_x = -normal.y.acos();
-        builder.add_shape(
-            &triangles[0],
-            material::Material::conductor_with_alpha(
-                [2.0, 2.0, 1.0],
-                [3.0, 3.0, 15.0],
-                alpha,
-                alpha,
-            ),
-            scene::InstanceTransform {
-                translation: center,
-                rotation: glam::Quat::from_rotation_x(rotation_x),
-                ..Default::default()
-            },
-        );
-        builder.add_shape(
-            &triangles[1],
-            material::Material::conductor_with_alpha(
-                [2.0, 2.0, 1.0],
-                [3.0, 3.0, 15.0],
-                alphas[i],
-                alphas[i],
-            ),
-            scene::InstanceTransform {
-                translation: center,
-                rotation: glam::Quat::from_rotation_x(rotation_x),
-                ..Default::default()
-            },
-        );
-    }
 
-    let ground = geometry::Plane::new(glam::vec3(0.0, -5.0, 0.0), glam::Vec3::Y, 100.0);
-    let wall = geometry::Plane::new(glam::vec3(0.0, 0.0, 15.0), glam::Vec3::NEG_Z, 100.0);
+    let ground = geometry::Plane::new(glam::Vec3::ZERO, glam::Vec3::Y, 100.0);
     builder.add_shape(
         &ground,
         material::Material::ground(glam::Vec3::ONE),
         Default::default(),
     );
-    builder.add_shape(
-        &wall,
-        material::Material::diffuse(glam::Vec3::ONE),
-        Default::default(),
-    );
+
     builder.add_uniform_infinite_light([0.5, 0.5, 0.5]);
 
     let scene = builder.build();
-
-    let mixture_light_sampler = light_sampler::LightSampler::<
-        light_sampler::MixtureLightSelector<
-            20,
-            light_sampler::UniformLightSelector,
-            light_sampler::PowerLightSelector,
-        >,
-    >::new(&scene, light_sampler::MisCompensation::Disabled);
 
     let light_sampler_compensated = light_sampler::LightSampler::<
         light_sampler::MixtureLightSelector<
@@ -133,35 +87,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         >,
     >::new(&scene, light_sampler::MisCompensation::Enabled);
 
-    let simple_path_tracing_integrator =
-        integrator::SimplePathTracingIntegrator::new(&mixture_light_sampler);
-
-    let path_tracing_integrator = integrator::PathTracingIntegrator::new(&mixture_light_sampler);
-
-    let path_tracing_integrator_compensated =
+    let path_tracing_integrator =
         integrator::PathTracingIntegrator::new(&light_sampler_compensated);
 
-    if integrator::preview(&path_tracing_integrator_compensated, &mut camera, &scene) {
-        integrator::render(
-            &simple_path_tracing_integrator,
-            &mut camera,
-            &scene,
-            64,
-            "PT_NEE.ppm",
-        )?;
+    if integrator::preview(&path_tracing_integrator, &mut camera, &scene) {
         integrator::render(
             &path_tracing_integrator,
             &mut camera,
             &scene,
             64,
             "PT_MIS.ppm",
-        )?;
-        integrator::render(
-            &path_tracing_integrator_compensated,
-            &mut camera,
-            &scene,
-            64,
-            "PT_MIS_COMPENSATION.ppm",
         )?;
     }
 
